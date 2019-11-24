007e59985a5e59cc763174d289f24a18514bd174
Jay Wen <jaywen89@gmail.com>
2019-11-24 18:29:39 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,6 +11,7 @@ end
 def print_first_directors_movie_titles
   row_index = 0
 while row_index < print_first_directors_movie_titles.length do
+  
   puts print_first_directors_movie_titles
 end
 end
