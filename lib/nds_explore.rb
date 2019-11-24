c4902c2683983c749bd75f4d3943662800f4ead6
Jay Wen <jaywen89@gmail.com>
2019-11-24 18:40:16 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,9 +9,20 @@ pp nds
 end
 
 def print_first_directors_movie_titles
-  movie_titles = 0
-while movie_titles < print_first_directors_movie_titles.length do
-  movie_titles += 1
-  puts print_first_directors_movie_titles
+#   movie_titles = 0
+# while movie_titles < print_first_directors_movie_titles.length do
+#   movie_titles += 1
+#   puts print_first_directors_movie_titles
+  
+  
+  row_index = 0
+while row_index < print_first_directors_movie_titles.each do
+  element_index = 0
+  while element_index < print_first_directors_movie_titles[row_index].each do
+    puts print_first_directors_movie_titles[row_index][element_index]
+    element_index += 1
+  end
+  row_index += 1
+end
 end
 end
